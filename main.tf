5f5216ebde2cdc543b9e39eba931dd00e3c8fa78
Ericlys Moreira <ericlysm@gmail.com>
2024-09-28 10:36:10 -0300

    chore: configure gcp terraform and vm resource

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "google_compute_instance" "vm" {
+  name         = "eric-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
